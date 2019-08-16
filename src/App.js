c305bf97d6bc1f3c804f9b56ce944a3f39183a59
zzphoghy <doivjpxx@gmail.com>
2019-08-16 16:20:00 +0700

    active link navlink

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './hoc/Layout/Layout';
 import { Route, Switch } from 'react-router-dom';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Order/Order';
 
 class App extends Component {
 
@@ -24,8 +25,9 @@ class App extends Component {
         {/* <BurgerBuilder />
         <Checkout /> */}
         <Switch>
-        <Route path="/" exact  component={BurgerBuilder}/>
         <Route path="/checkout" component={Checkout}/>
+        <Route path="/orders" component={Orders} />
+        <Route path="/" exact  component={BurgerBuilder}/>
         </Switch>
       </Layout>
     </div>
