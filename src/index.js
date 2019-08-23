f2dbe586be0c4775ca80ff466a9520d4a2bf5b47
zzphoghy <doivjpxx@gmail.com>
2019-08-23 11:10:05 +0700

    split to action creators

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import reducer from "./store/reducer";
+import reducer from "./store/reducers/burgerBuilder";
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
